5fa1c346e9fadd2fc0b663830229b5c15a228ebc
gksmfly <149565853+gksmfly@users.noreply.github.com>
2025-11-06 21:47:58 +0900

    Update Point.kt

--- src/main/kotlin/rts/Point.kt
+++ src/main/kotlin/rts/Point.kt
@@ -1,5 +1,6 @@
 package rts
 
-data class Point(val x: Int, val y: Int) {
-    override fun toString(): String = "(${x}, ${y})"
-}
+data class Point(val x: Int, val y: Int)
+
+/** 좌표 출력용 */
+fun Point.fmt(): String = "(${this.x}, ${this.y})"
