299267a3f92ee9628433719f931267aedb82d5a0
gksmfly <149565853+gksmfly@users.noreply.github.com>
2025-11-06 21:48:17 +0900

    Update Abstractions.kt

--- src/main/kotlin/rts/Abstractions.kt
+++ src/main/kotlin/rts/Abstractions.kt
@@ -1,17 +1,27 @@
 package rts
 
+/** 지상/공중 도메인 */
 enum class Domain { GROUND, AIR }
 
+/** (선택) 종족 – 과제 확장 대비 */
+enum class Race { HUMAN, FANTASY, AIRBORNE }
+
+/** 이동 역할 */
 interface Movable {
     fun moveTo(target: Point)
 }
 
+/** 공격 역할 */
 interface Attacker {
     fun attack(target: UnitBase)
 }
 
-open class UnitBase(
-    val name: String,
-    var position: Point,
-    open val domain: Domain
-)
+/** 이동 전략 */
+interface MoveStrategy {
+    fun move(self: UnitBase, to: Point)
+}
+
+/** 공격 전략 */
+interface AttackStrategy {
+    fun attack(self: UnitBase, target: UnitBase)
+}
